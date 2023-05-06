08407be1e9f6373ca047a162a926df7caafc15e2
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-05-06 11:34:05 +0300

    Add React Query To Project

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 import '../styles/globals.scss'
 import type {AppProps} from 'next/app'
+import {QueryClientProvider} from "@tanstack/react-query";
+import {queryClient} from "@config/queryClient";
 
 export default function App({Component, pageProps}: AppProps) {
-    // @ts-ignore
-    return <Component {...pageProps} />
+    return (
+        <QueryClientProvider client={queryClient}>
+            {/* @ts-ignore*/}
+            <Component {...pageProps} />
+        </QueryClientProvider>
+    )
 }
