768c837eac9a165d3f3d137f4afc6fe652b32c07
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-05-07 20:34:54 +0300

    Create Providers Folder

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.scss'
 import type {AppProps} from 'next/app'
 import {QueryClientProvider} from "@tanstack/react-query";
-import {queryClient} from "@config/queryClient";
+import {queryClient} from "@providers/queryClient";
 
 export default function App({Component, pageProps}: AppProps) {
     return (
