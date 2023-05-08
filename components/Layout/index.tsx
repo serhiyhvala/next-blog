5ce7982dbee2c233d07d31e5304fd0c2fb61c151
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-05-08 08:43:54 +0300

    Add favicons

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -15,6 +15,9 @@ const Layout: FC<LayoutProps> = ({children, title}) => {
         <>
             <Head>
                 <title>{concatTitle(title)}</title>
+                <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />  
+                <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />  
+                <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
             </Head>
             <Navbar/>
             <div className={styles.wrapper}>
